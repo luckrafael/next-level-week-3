a2e110a7c36d2d20719f34ccee9221aa2b1b2f97
luckrafael <lrs.rafael@hotmail.com>
2020-12-30 16:07:32 -0300

    Visual do mapa em dark

--- frontend/src/pages/CreateOrphanage.tsx
+++ frontend/src/pages/CreateOrphanage.tsx
@@ -96,7 +96,7 @@ async function handleSubmit(event: FormEvent){
               onClick={handleMapClick}
             >
               <TileLayer 
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
               {position.latitude !== 0 && (
